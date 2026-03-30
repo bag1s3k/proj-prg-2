398b9276d9e68116a647799ca47b1cdb0eb3a531
bag1s3k <jetam.mravenec@gmail.com>
2026-03-30 20:47:05 +0200

    Reorganization of the project (use PHP)

--- index.php
+++ index.php
@@ -4,34 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="modules/nav/topnav.css">
     <link rel="icon" href="images/icons/favicon.png" type="image/x-icon">
 
     <title>Lendly</title>
 </head>
 <body>
-    <div id="nav-container" class="center-flex">
-        <nav class="fixed-width center-flex">
-            <a href="./index.html"  class="href-clean">
-                <div class="center-flex">
-                    <img src="images/icons/favicon.png" alt="">
-                    <h1>Lendly</h1>
-                </div>
-            </a>
-
-            <div id="hrefs">
-                <a class="href-clean" href="http://">Products</a>
-                <a class="href-clean" href="http://">Borrowed</a>
-                <a class="href-clean" href="http://">Contact</a>
-                <a class="href-clean" href="http://">Notifications</a>
-            </div>
-
-            <div class="center-flex">
-                <img src="images/icons/shopping-cart.png" alt="">
-                <img src="images/icons/account-nologin.png" alt="">
-            </div>
-
-        </nav>
-    </div>
+    <?php include "modules/nav/topnav.php"; ?>
 
     <div id="first-page">
         <div>
---
+++ modules/nav/topnav.php
@@ -0,0 +1,23 @@
+<div id="nav-container" class="center-flex">
+    <nav class="fixed-width center-flex">
+        <a href="./index.html"  class="href-clean">
+            <div class="center-flex">
+                <img src="images/icons/favicon.png" alt="">
+                <h1>Lendly</h1>
+            </div>
+        </a>
+
+        <div id="hrefs">
+            <a class="href-clean" href="http://">Products</a>
+            <a class="href-clean" href="http://">Borrowed</a>
+            <a class="href-clean" href="http://">Contact</a>
+            <a class="href-clean" href="http://">Notifications</a>
+        </div>
+
+        <div class="center-flex">
+            <img src="images/icons/shopping-cart.png" alt="">
+            <img src="images/icons/account-nologin.png" alt="">
+        </div>
+
+    </nav>
+</div>
