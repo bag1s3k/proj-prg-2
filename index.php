8a028f804bdd10f2f2b329d245e990f7f9916fb2
bag1s3k <jetam.mravenec@gmail.com>
2026-03-31 21:59:23 +0200

    Add footer

--- index.php
+++ index.php
@@ -48,8 +48,38 @@
         </div>
     </div>
 
-    <footer style="height: 50em;background:grey">
-        <h1>Fotter</h1>
+    <footer>
+        <h1>Lendly</h1>
+        <div id="hrefs">
+            <a href="#" class="href-clean">About</a>
+            <a href="#" class="href-clean">Services</a>
+            <a href="#" class="href-clean">Press</a>
+            <a href="#" class="href-clean">Careers</a>
+            <a href="#" class="href-clean">FAQ</a>
+            <a href="#" class="href-clean">Legal</a>
+            <a href="#" class="href-clean">Contact</a>
+        </div>
+
+        <h2>Stay in touch</h2>
+        <div id="socials">
+            <a href="#">
+                <img src="images/icons/instagram.png" alt="">
+            </a>
+            <a href="#">
+                <img src="images/icons/facebook.png" alt="">
+            </a>
+            <a href="#">
+                <img src="images/icons/twitter.png" alt="">
+            </a>
+            <a href="#">
+                <img src="images/icons/pinterest.png" alt="">
+            </a>
+            <a href="#">
+                <img src="images/icons/reddit.png" alt="">
+            </a>
+        </div>
+
+        <p>© bag1s3k all rights reserverd</p>
     </footer>
 </body>
 </html>
