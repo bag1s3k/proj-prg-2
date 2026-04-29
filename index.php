a0aad4afc2881410a68b989ca4e1f988670753a5
bag1s3k <jetam.mravenec@gmail.com>
2026-04-29 18:19:33 +0200

    Display products from DB also on index page

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
     require_once "config.php";
     session_start();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_id'])) {
+        $delete_id = $_POST['delete_id'];
+        $del_query = "DELETE FROM products WHERE id = ?";
+        $stmt = mysqli_prepare($con, $del_query);
+        mysqli_stmt_bind_param($stmt, "i", $delete_id);
+        mysqli_stmt_execute($stmt);
+        header("Location: account.php");
+        die;
+    }
+
+    $query = "SELECT * FROM products ORDER BY id DESC";
+    $result = mysqli_query($con, $query);
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +24,7 @@
     
     <link rel="stylesheet" href="<?php echo url('style.css'); ?>">
     <link rel="stylesheet" href="<?php echo url('modules/nav/topnav.css'); ?>">
+    <link rel="stylesheet" href="<?php echo url('modules/account/account.css'); ?>">
 
     <link rel="icon" href="<?php echo url('images/icons/favicon.png'); ?>" type="image/x-icon">
 
@@ -36,20 +50,26 @@
     </nav>
 
     <div id="cards-container" class="center-flex fixed-width">
-        <div class="card">
-            <img src="<?php echo url('images/sports-images/soccer-ball.jpg'); ?>" alt="soccer ball">
-            <div id="description">
-                <h2>Soccer ball</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias</p>
-                <div id="bottom">
-                    <p>50 KČ/h</p>
-                    <button class="center-flex">
-                        <img src="<?php echo url('images/icons/shopping-cart.png'); ?>">
-                        <p>Add</p>
-                    </button>
+        <?php if ($result && mysqli_num_rows($result) > 0): ?>
+            <?php while($row = mysqli_fetch_assoc($result)): ?>
+                <div class="card">
+                    <img src="<?php echo htmlspecialchars($row["image_url"]); ?>">
+                    <div id="description">
+                        <h2><?php echo htmlspecialchars($row['title']); ?></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias</p>
+                        <div id="bottom">
+                            <p>50 KČ/h</p>
+                            <button class="center-flex">
+                                <img src="<?php echo url('images/icons/shopping-cart.png'); ?>">
+                                <p>Add</p>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <p>Žádné produkty k zobrazení.</p>
+        <?php endif; ?>
     </div>
 
     <?php include "modules/footer/footer.php"; ?>
