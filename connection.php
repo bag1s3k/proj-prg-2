8c2a2ce375af9e42caf7bbfb626229e186f23e5b
bag1s3k <jetam.mravenec@gmail.com>
2026-05-14 13:09:31 +0200

    Upload example of connection to database

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "lendly";
+
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
+    die("Failed to connect to database!");
+}
